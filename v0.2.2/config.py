42040b099be73cbd59647d9d4878f64398c227f4
Advaith Sanil Kumar <advaithsanilkumar@gmail.com>
2025-01-11 10:24:05 +0530

    Modularized v0.2.2, minor fixes
    - Split the code into multiple files based on functionality
    - Fixed issue with URL scraping function where the content was not added to the vector database even though it was scraped correctly
    - Fixed issue with "Start Conversation" where it would ask you to upload a file/URL for both company and user, even if both had been uploaded

---
+++ v0.2.2/config.py
@@ -0,0 +1,16 @@
+import os
+import secret
+
+AZURE_ENDPOINT = secret.AZURE_ENDPOINT
+AZURE_API_KEY = secret.AZURE_KEY
+AZURE_API_VERSION = "2024-05-01-preview"
+AZURE_DEPLOYMENT = "gpt40-mini-long-context"
+AZURE_EMBEDDING_DEPLOYMENT = "embedding"
+
+PERSIST_DIRECTORY = os.path.join(os.getcwd(), "chroma_storage")
+os.makedirs(PERSIST_DIRECTORY, exist_ok=True)
+
+CURRENT_DIRECTORY = os.getcwd()
+LOGO_PATH = os.path.join(CURRENT_DIRECTORY, "images", "logo_transparent.png")
+CAZE_PATH = os.path.join(CURRENT_DIRECTORY, "images", "caze_logo_white_trans.png")
+ICON_PATH = os.path.join(CURRENT_DIRECTORY, "images", "icon.png")
