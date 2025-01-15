6ff00e26ee2e1bb6bee120acee88e8abfa08af7d
Joval Kuruvila <115721992+Behind-mask@users.noreply.github.com>
2025-01-15 15:59:05 +0530

    Clean up of previous versions v0.2.1,v0.2.2

--- v0.2.1/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    PERSIST_DIRECTORY = os.path.join(os.getcwd(), "chroma_storage")
-    OPENAI_MODEL = "gpt-4-turbo-preview"
-    EMBEDDING_MODEL = "text-embedding-ada-002"
-    TEMPERATURE = 0.2
-
